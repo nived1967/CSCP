a15a4f79ddadce828c4191e99b58160d072ad9a6
unnimayat <88229921+unnimayat@users.noreply.github.com>
2022-10-28 16:30:51 +0530

    new changes

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -3,8 +3,38 @@ import './about.styles.css'
 const About=()=>
 {
     return(
-       <div>
+       <div className='about'>
+        <h1 className='head'>ABOUT THE CLUB</h1>
+
+        <div className='new1'>
+        <div className='x'>
+        <h1 className='goal'>Goal</h1>
+        <br></br>
+        <p1 className='discription-goal1'>An initiative by a group of<br></br>cybersecurity and competitive<br></br>programming enthusiasts at <br></br>College Of Enginneering, <br></br>Trivandrum</p1>
+        <br></br>
+        </div>
+
+        <div className='y'>
+        <h1 className='objectives'>Word from the founder</h1>
+        <br></br>
+        <p1 className='discription-goal1'> " An initiative by a group of <br></br>cybersecurity and competitive <br></br>programming enthusiasts at <br></br>College Of Enginneering, <br></br>Trivandrum "</p1>
+        </div>
+        </div>
         
+        <div className='new2'>
+        <div className='x'>
+        <h1 className='objectives'>Objectives</h1>
+        <br></br>
+        <p1 className='discription-goal1'>An initiative by a group of<br></br>cybersecurity and competitive<br></br>programming enthusiasts at <br></br>College Of Enginneering, <br></br>Trivandrum</p1>
+        <br></br>
+        </div>
+
+        <div className='y'>
+        <h1 className='goal'>Action Plan</h1>
+        <br></br>
+        <p1 className='discription-goal1'>An initiative by a group of<br></br>cybersecurity and competitive<br></br>programming enthusiasts at <br></br>College Of Enginneering, <br></br>Trivandrum</p1>
+        </div>
+        </div>
        </div>
     )
 }
