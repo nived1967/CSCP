76673b0a6c7f031cae7fecd0b6aa5c88a78a6b0b
unnimayat <88229921+unnimayat@users.noreply.github.com>
2022-10-29 00:40:19 +0530

    new changes

--- src/components/about/about.component.jsx
+++ src/components/about/about.component.jsx
@@ -21,7 +21,7 @@ const About=()=>
         </div>
         </div>
         
-        <div className='new2'>
+        <div className='new1'>
         <div className='x'>
         <h1 className='objectives'>Objectives</h1>
         <br></br>
@@ -35,6 +35,12 @@ const About=()=>
         <p1 className='discription-goal1'>An initiative by a group of<br></br>cybersecurity and competitive<br></br>programming enthusiasts at <br></br>College Of Enginneering, <br></br>Trivandrum</p1>
         </div>
         </div>
+        <br></br>
+        <div className='new1'>
+        <h1 className='community'>COMMUNITY</h1>
+        <p1 className='discription-goal2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor<br></br>incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis <br></br>nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br></br>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu <br></br>fugiat nulla pariatur.</p1>
+        </div>
+
        </div>
     )
 }
--- src/components/community/community.component.jsx
+++ src/components/community/community.component.jsx
@@ -1,8 +1,23 @@
 import './community.styles.css'
+import poster1 from './poster1 1.png'
+import poster2 from './poster2 1.png'
+import poster3 from './poster3 1.png'
 
 const Community = () =>{
-    return(
-        <h1>Community</h1>
+    return( 
+    <div>
+        <div className='new1'>
+        <h1 className='community'>COMMUNITY</h1>
+        <p1 className='discription-goal2'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor<br></br>incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis <br></br>nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. <br></br>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu <br></br>fugiat nulla pariatur.</p1>
+        </div>
+        <br></br>
+        <br></br>
+        <div className='images1'>
+            <div className='images2'><img src={poster1} alt="poster1"></img></div>
+            <div className='images2'><img src={poster2} alt="poster2"></img></div>
+            <div className='images2'><img src={poster3} alt="poster3"></img></div>
+        </div>
+    </div>
     )
 }
 
