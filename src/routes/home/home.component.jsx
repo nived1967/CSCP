a6d38ef0ea861594ec6a319231ca152cdd0624d3
Akshay Raj <akrcetcs@gmail.com>
2022-10-29 19:06:50 +0530

    bg change

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -4,14 +4,22 @@ const Home = () =>
 {
 return(
     <div className='home'>
-    <div>
+        <div className="row">
+            <div className="left">.</div>
+            <div className="right">.</div>
+
+
+        </div>
+        
+    
+       
     <h3>CYBERSECURITY AND<br></br> COMPETITIVE <br></br>PROGRAMMING CLUB</h3>
-            <h2>An initiative by a group of cybersecurity and competitive<br></br>programming enthusiasts at College Of Enginneering,<br></br>Trivandrum</h2>
+            <h2>An initiative by a group of cybersecurity and competitive<br></br>
+            programming enthusiasts at College Of Engineering,<br></br>Trivandrum</h2>
             <button className="Button">Latest Activities</button>
     </div>
-    <div>
-    </div>
-    </div>
+   
+   
 )
 }
 
