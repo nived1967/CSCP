1d2af145347b28797f61887e078dd76466f4697c
unnimayat <88229921+unnimayat@users.noreply.github.com>
2022-10-29 19:18:38 +0530

    new changes added

--- src/components/activities/activities.component.jsx
+++ src/components/activities/activities.component.jsx
@@ -3,7 +3,22 @@ import './activities.styles.css'
 const Activities = () =>
 {
     return(
-        <h1>Activities</h1>
+        <div>
+        <h1 className='activities'>ACTIVITIES</h1>
+        <h1 className='blogs'>Blogs</h1>
+        <br></br>
+        <p1 className='discription-blogs'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt <br></br>ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation <br></br>ullamco laboris nisi ut aliquip ex.</p1>
+        <br></br>
+        <div className='both'>
+        <div className='lightblue'>
+            <h1 className='text'>The State of <br></br>Security</h1>
+        </div>
+        <br></br>
+        <div className='darkblue'>
+            <h1 className='text'>Roadmap to<br></br>Competitive<br></br>Programming</h1>
+        </div>
+        </div>
+        </div>
     )
 }
 
