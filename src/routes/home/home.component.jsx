e9c5ec96730431a9dc1b80040d2740fb992b00df
unnimayat <88229921+unnimayat@users.noreply.github.com>
2022-11-12 16:36:49 +0530

    new changes added

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -7,12 +7,7 @@ return(
         <div className="row">
             <div className="left">.</div>
             <div className="right">.</div>
-
-
-        </div>
-        
-    
-       
+    </div> 
     <h3>CYBERSECURITY AND<br></br> COMPETITIVE <br></br>PROGRAMMING CLUB</h3>
             <h2>An initiative by a group of cybersecurity and competitive<br></br>
             programming enthusiasts at College Of Engineering,<br></br>Trivandrum</h2>
